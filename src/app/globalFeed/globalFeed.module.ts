c2561710f4cb24a06ded235913ab47a254a90bfa
Zhanna <nesterova.zhannet@gmail.com>
2023-07-24 14:13:14 +0500

    popular tags module creation

--- src/app/globalFeed/globalFeed.module.ts
+++ src/app/globalFeed/globalFeed.module.ts
@@ -4,6 +4,7 @@ import { GlobalFeedComponent } from './components/globalFeed/globalFeed.componen
 import { RouterModule } from '@angular/router';
 import { FeedModule } from '../shared/modules/feed/feed.module';
 import { BannerModule } from '../shared/modules/banner/banner.module';
+import { PopularTagsModule } from '../shared/modules/popularTags/popularTags.module';
 
 const routes = [
   {
@@ -18,6 +19,7 @@ const routes = [
     RouterModule.forChild(routes),
     FeedModule,
     BannerModule,
+    PopularTagsModule,
   ],
   declarations: [GlobalFeedComponent],
 })
