9d65f6003064e2fc6b0ec37954c68c9b87c0dff6
Zhanna <nesterova.zhannet@gmail.com>
2023-08-01 15:30:17 +0500

    currentUser settings page creation

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { AuthResponseInterface } from '../types/authResponse.interface';
 import { LoginRequestInterface } from '../types/loginRequest.interface';
+import { CurrentUserInputInterface } from 'src/app/shared/types/currentUserInput.interface';
 
 @Injectable()
 export class AuthService {
@@ -34,5 +35,12 @@ export class AuthService {
     const url = `${environment.apiUrl}/user`;
 
     return this.http.get(url).pipe(map(this.getUser));
+  }
+
+  updateCurrentUser(
+    currentUserInput: CurrentUserInputInterface
+  ): Observable<CurrentUserInterface> {
+    const url = environment.apiUrl + '/user';
+    return this.http.put(url, currentUserInput).pipe(map(this.getUser));
   }
 }
---
+++ src/app/settings/settings.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SettingsComponent } from './components/settings/settings.component';
+import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from 'src/app/settings/store/reducers';
+
+const routes = [
+  {
+    path: 'settings',
+    component: SettingsComponent,
+  },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    StoreModule.forFeature('settings', reducers),
+  ],
+  declarations: [SettingsComponent],
+})
+export class SettingsModule {}
