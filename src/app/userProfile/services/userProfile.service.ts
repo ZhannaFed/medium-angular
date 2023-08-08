d493835b3ee1b9040403b5055e629143f20f3b93
Zhanna <nesterova.zhannet@gmail.com>
2023-08-08 15:12:37 +0500

    user profile page rendering

--- src/app/userProfile/services/userProfile.service.ts
+++ src/app/userProfile/services/userProfile.service.ts
@@ -10,7 +10,7 @@ export class UserProfileService {
   constructor(private http: HttpClient) {}
 
   getUserProfile(slug: string): Observable<ProfileInterface> {
-    const url = `${environment.apiUrl}/profile/${slug}`;
+    const url = `${environment.apiUrl}/profiles/${slug}`;
 
     return this.http
       .get(url)
